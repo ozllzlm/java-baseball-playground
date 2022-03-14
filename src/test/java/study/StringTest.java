5c8e01be5e6972bedb5e4444d8f3344123a2c3ae
JangHyunMoon <abc9529@naver.com>
2022-03-14 22:28:11 +0900

    String split 함수 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -4,10 +4,26 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringTest {
+public class
+StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+
+        String ex = "1,2";
+        String ex2 = "1";
+
+        String[] exArray = ex.split(",");
+        String[] exArray2 = ex2.split(",");
+
+        assertThat(exArray).contains("1");
+        assertThat(exArray).contains("2");
+        assertThat(exArray).containsExactly("1", "2");
+        assertThat(exArray2).containsExactly("1");
     }
 }
